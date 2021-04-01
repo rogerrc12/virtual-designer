34fb4c4bfdc66edb5d500ea269c39357683f22c9
Roger Rengifo <thelea12@gmail.com>
2021-04-01 14:09:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Trailer from './components/Trailer';
+import ColorPicker from './components/ColorPicker';
+// import SlashStyle from './components/SlashStyle';
+import PDFCreation from './components/PDFCreation';
+
+import classes from './App.module.scss';
 
 function App() {
+  const [frontColor, setFrontColor] = useState('');
+  const [rearColor, setRearColor] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={classes.MainWrapper}>
+      <h1>Virtual Designer</h1>
+      <div className='rounded-xl mt-8 w-2/3 m-auto py-10'>
+        <Trailer rearColor={rearColor} frontColor={frontColor} />
+        <div className='flex items-center justify-between px-20'>
+          <ColorPicker label='Rear color' color={rearColor} setColor={setRearColor} />
+          <ColorPicker label='Front color' color={frontColor} setColor={setFrontColor} />
+        </div>
+        <div className='px-20'>
+          <h1 className='mt-12'>CREATE YOUR WORKSHEET</h1>
+          <hr className='border-dashed my-4' />
+          <p>
+            Once you've choosen your design, you can put together a worksheet to take to your local dealer to get the trailer you're looking for! Simply select the trailer model
+            you're interested in below to see available options for it. Then, select the options you're interested in and print off your selection!
+          </p>
+        </div>
+        <PDFCreation />
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/PDFCreation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PDFCreation = () => {
+  return (
+    <div className='flex justify-center my-8'>
+      <button onClick={() => {}} className='uppercase font-bold py-4 px-12 bg-green-500 border border-white rounded'>
+        CRRATE YOUR WORKSHEET
+      </button>
+    </div>
+  );
+};
+
+export default PDFCreation;
