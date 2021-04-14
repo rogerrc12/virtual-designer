f2b50d01bd779894b4c72bb3a5daa0c0c9a81304
Roger Rengifo <thelea12@gmail.com>
2021-04-14 14:09:41 -0500

    white bg

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import Trailer from './components/Trailer';
 import ColorPicker from './components/ColorPicker';
 import imgs from './images';
-import PDFCreation from './components/PDFCreation';
 import { cacheImages } from './cacheImages';
 
 import Spinner from './components/Spinner';
@@ -11,8 +10,8 @@ import Spinner from './components/Spinner';
 import classes from './App.module.scss';
 
 function App() {
-  const [frontColor, setFrontColor] = useState('');
-  const [rearColor, setRearColor] = useState('');
+  const [frontColor, setFrontColor] = useState('gloss-white');
+  const [rearColor, setRearColor] = useState('gloss-white');
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -22,26 +21,30 @@ function App() {
   return (
     <main className={classes.MainWrapper}>
       <h1>Virtual Designer</h1>
-      <div className='rounded-xl mt-8 w-2/3 m-auto py-10'>
+      <div className='rounded-xl mt-8 w-full md:w-2/3 m-auto py-10 px-4'>
         {isLoading && <Spinner />}
         {!isLoading && (
           <>
             <Trailer rearColor={rearColor} frontColor={frontColor} />
-            <div className='flex items-center justify-between px-20'>
+            <div className='flex flex-wrap items-center justify-between pr-20 pl-16'>
               <ColorPicker label='Rear color' color={rearColor} setColor={setRearColor} />
               <ColorPicker label='Front color' color={frontColor} setColor={setFrontColor} />
             </div>
           </>
         )}
         <div className='px-20'>
-          <h1 className='mt-12'>CREATE YOUR WORKSHEET</h1>
+          <h2 className='mt-12'>Visualize Your CORE</h2>
           <hr className='border-dashed my-4' />
           <p>
-            Once you've choosen your design, you can put together a worksheet to take to your local dealer to get the trailer you're looking for! Simply select the trailer model
-            you're interested in below to see available options for it. Then, select the options you're interested in and print off your selection!
+            The "Virtual Designer" displays the color selections for the CORE ICE 8019 ATX. 6511 and 6515 Models only have one solid color choice for the exterior of the unit.
+            6519, 8019, and 8025 Models have multiple color choices.
           </p>
         </div>
-        <PDFCreation />
+        <div className='flex justify-center my-8'>
+          <a href='https://core-ice.com/models/' className='uppercase font-bold py-4 px-12 border border-black text-white rounded' style={{ backgroundColor: '#24317A' }}>
+            See Models
+          </a>
+        </div>
       </div>
     </main>
   );
--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -4,41 +4,27 @@ import Color from './Color';
 
 import classes from './ColorPicker.module.scss';
 
-const colors = [
-  'non-gelcoat',
-  'gloss-white',
-  'matte-elder-white',
-  'matte-greige',
-  'gloss-med-gray',
-  'matte-charcoal',
-  'matte-black',
-  'gloss-orange',
-  'matte-red',
-  'matte-dark-blue',
-];
+const colors = ['gloss-white', 'matte-elder-white', 'gloss-med-gray', 'matte-charcoal', 'matte-black', 'gloss-orange', 'matte-red', 'matte-dark-blue'];
 
 const ColorPicker = ({ setColor, color, label }) => {
   const onSelectChange = (e) => setColor(e.target.value);
 
   return (
-    <div className='w-2/5'>
+    <div className='w-full md:w-2/5 my-4'>
       <div className='flex items-center justify-between w-full'>
         <p className='mr-3 uppercase'>{label}:</p>
         <select
           className={`block appearance-none w-1/2 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 ${classes.Select}`}
           onChange={onSelectChange}
           value={color}>
-          <option value=''>-</option>
-          <option value='gloss-white'>Gloss white</option>
-          <option value='non-gelcoat'>Non gelcoat</option>
+          <option value='gloss-white'>Arctic White Gloss</option>
           <option value='matte-elder-white'>Matte Elder White</option>
-          <option value='matte-greige'>Matte Greige</option>
-          <option value='gloss-med-gray'>Gloss Med Gray</option>
-          <option value='matte-charcoal'>Matte Charcoal</option>
-          <option value='matte-black'>Matte Black</option>
-          <option value='gloss-orange'>Gloss Orange</option>
-          <option value='matte-red'>Matte Red</option>
-          <option value='matte-dark-blue'>Matte Dark Blue</option>
+          <option value='gloss-med-gray'>Storm Grey Gloss</option>
+          <option value='matte-charcoal'>Tundra Night Matte</option>
+          <option value='matte-black'>Carbon Black Matte</option>
+          <option value='gloss-orange'>Solar Orange Gloss</option>
+          <option value='matte-red'>Race Red Matte</option>
+          <option value='matte-dark-blue'>Deep Sea Blue Matte</option>
         </select>
       </div>
       <hr className='border-dashed mt-4' />
